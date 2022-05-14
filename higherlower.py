2c7e126aefe2b58c9010cc7b4433e179d37aba6e
unknown <22891566@student.uwa.edu.au>
2022-05-14 21:54:15 +0800

    minor fix

--- higherlower.py
+++ higherlower.py
@@ -3,4 +3,4 @@ from app.models import User, Location
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Location': Location}
+    return {'db': db, 'User': User, 'Location': Location}
