64ff86bed35ae14d66252fb30a69504ca8fde48e
unknown <22891566@student.uwa.edu.au>
2022-05-17 23:11:13 +0800

    more fixes from the great merge disaster of 2022

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from app.forms import RegistrationForm
 from flask import render_template, flash, redirect, request, url_for
 from app.forms import LoginForm
-from app.models import User
+from app.models import User, Location
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
