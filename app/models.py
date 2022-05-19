89244a2be820e9b7ad44aeb6fae42a4179eff41e
Jacob12245 <22891566@student.uwa.edu.au>
2022-05-19 18:13:06 +0800

    added structure for 3 leaderboards

--- app/models.py
+++ app/models.py
@@ -12,9 +12,15 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    high_score = db.Column(db.Integer)
-    average_score = db.Column(db.Float)
-    number_of_plays = db.Column(db.Integer)
+    high_score_easy = db.Column(db.Integer)
+    average_score_easy = db.Column(db.Float)
+    number_of_plays_easy = db.Column(db.Integer)
+    high_score_normal = db.Column(db.Integer)
+    average_score_normal = db.Column(db.Float)
+    number_of_plays_normal = db.Column(db.Integer)
+    high_score_hard = db.Column(db.Integer)
+    average_score_hard = db.Column(db.Float)
+    number_of_plays_hard = db.Column(db.Integer)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
